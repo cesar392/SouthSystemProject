ff63922a260f500caf3bdab21b47a6ec91e30a2d
Cesar Comelli <cesarhccomelli@gmail.com>
2022-07-31 17:47:56 -0300

    Add cocoapods to the project

---
+++ SouthSystemProject/Pods/Target Support Files/Pods-SouthSystemProject-SouthSystemProjectUITests/Pods-SouthSystemProject-SouthSystemProjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SouthSystemProject_SouthSystemProjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SouthSystemProject_SouthSystemProjectUITests
+@end
---
+++ SouthSystemProject/Pods/Target Support Files/Pods-SouthSystemProject/Pods-SouthSystemProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SouthSystemProject : NSObject
+@end
+@implementation PodsDummy_Pods_SouthSystemProject
+@end
