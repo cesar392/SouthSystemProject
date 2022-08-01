1eb87ede82716aaca7a4a237d1e00186b251f05e
Cesar Comelli <cesarhccomelli@gmail.com>
2022-08-01 20:03:17 -0300

    Add notification banner to the project

--- SouthSystemProject/Pods/Target Support Files/Pods-SouthSystemProject/Pods-SouthSystemProject-frameworks.sh
+++ SouthSystemProject/Pods/Target Support Files/Pods-SouthSystemProject/Pods-SouthSystemProject-frameworks.sh
@@ -178,12 +178,18 @@ code_sign_if_enabled() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AloeStackView/AloeStackView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/LBTATools/LBTATools.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MarqueeLabel/MarqueeLabel.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/NotificationBannerSwift/NotificationBannerSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/R.swift.Library/Rswift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AloeStackView/AloeStackView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/LBTATools/LBTATools.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MarqueeLabel/MarqueeLabel.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/NotificationBannerSwift/NotificationBannerSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/R.swift.Library/Rswift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
